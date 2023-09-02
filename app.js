6ed9195deb6996f506e188fa9522703eee7cb794
Abul Khoyer <84437365+khoyer-authlab@users.noreply.github.com>
2023-09-02 22:53:58 +0600

    Update app.js

--- app.js
+++ app.js
@@ -30,22 +30,3 @@ video.addEventListener("pause", function () {
 video.addEventListener("contextmenu", function (event) {
   event.preventDefault();
 });
-
-var chatbox = document.getElementById('fb-customer-chat');
-chatbox.setAttribute("page_id", "102986075895269");
-chatbox.setAttribute("attribution", "biz_inbox");
-
-window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v17.0'
-        });
-      };
-
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
